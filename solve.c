407723446b0f0faa8730ad247850c3bd4f9b8854
Sebastian Hirnschall <sebastian@hirnschall.net>
2018-11-01 15:25:41 +0100

    added vectors for multiplication and addition and logic for resizing

--- solve.c
+++ solve.c
@@ -6,9 +6,28 @@
 
 #include <stdlib.h>
 
+#define DEFAULT_SIZE 20 //default vector length
+
+int *mult = NULL;
+int multc = DEFAULT_SIZE;
+int *add = NULL;
+int addc = DEFAULT_SIZE;
 
 double solve(char *calc) {
+    //if vectors are empty malloc
+    if (!mult)
+        mult = malloc(DEFAULT_SIZE * sizeof(int));
+    if (!add)
+        add = malloc(DEFAULT_SIZE * sizeof(int));
 
-
+    //if vectors are longer than DEFAULT_SIZE realloc
+    if (multc > DEFAULT_SIZE) {
+        realloc(mult, DEFAULT_SIZE * sizeof(int));
+        multc = DEFAULT_SIZE;
+    }
+    if (addc > DEFAULT_SIZE) {
+        realloc(add, DEFAULT_SIZE * sizeof(int));
+        addc = DEFAULT_SIZE;
+    }
 }
 

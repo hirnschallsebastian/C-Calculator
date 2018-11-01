67a519360860bd229b8e3f08384b07b8414bab03
Sebastian Hirnschall <sebastian@hirnschall.net>
2018-11-01 14:54:59 +0100

    initial commit

---
+++ solve.c
@@ -0,0 +1,14 @@
+//
+// Created by Sebastian Hirnschall on 01.11.18.
+//
+
+#include "solve.h"
+
+#include <stdlib.h>
+
+
+double solve(char *calc) {
+
+
+}
+
---
+++ solve.h
@@ -0,0 +1,10 @@
+//
+// Created by Sebastian Hirnschall on 01.11.18.
+//
+
+#ifndef C_CALCULATOR_SOLVE_H
+#define C_CALCULATOR_SOLVE_H
+
+double solve(char *);
+
+#endif //C_CALCULATOR_SOLVE_H

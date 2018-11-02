b7500a7be470793ca4b5397dd72162dec85e9819
Sebastian Hirnschall <sebastian@hirnschall.net>
2018-11-02 12:21:28 +0100

    switched from scanf to gnu-readline now supported: -shell-prompt -command-history -navigating left/right with arrow keys

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include "io.h"
 #include "solve.h"
 
-#define MAX_CALC_LENGTH 200
 
-int main() {
-    char calc[MAX_CALC_LENGTH];
+int main(int argc, char *argv[]) {
+    char *input, prompt[3];
+
+    snprintf(prompt, sizeof(prompt), "> ");
+    using_history();
 
     while (1) {
-        scanf(" %[^\n]s", calc); //scan with whitespaces
-        if (!strcmp(calc, "clear"))
+        input = readline(prompt);
+        if (!input)
+            continue;
+        add_history(input);
+
+        if (!strcmp(input, "clear"))
             clear();
-        else if (!strcmp(calc, "exit"))
+        else if (!strcmp(input, "exit"))
             return 0;
         else
-            printf("=%f\n", solve(calc, (int) strlen(calc)));
+            printf(" =%f\n", solve(input, (int) strlen(input)));
+        free(input);
     }
     return 0;
 }

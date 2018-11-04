2bf083705a322d493c895c4addf8fb950662c3c1
Sebastian Hirnschall <sebastian@hirnschall.net>
2018-11-05 00:47:46 +0100

    changed scaling... now x and y axes are no longer fixed in the middle. they can move. in fact the cross is now gone completely. instead a box and a grid is drawn. y axes numbers work.

--- plot.c
+++ plot.c
@@ -17,8 +17,8 @@
 
 #define WIDTH 100
 #define HEIGHT 30
-#define Y_INTERVALL 3
-#define X_INTERVALL 10
+#define Y_INTERVALL 5
+#define X_INTERVALL 20
 
 
 extern double x;
@@ -27,7 +27,7 @@ int plot(char *input, int start) {
     // parse input
     int i, j, k, argc = 0;
     double min = -10, max = 10, ymin = 9999, ymax = -9999, yIntervall, intervall;
-    double y[WIDTH];
+    double y[WIDTH + 1];
     char tmp[20];
     int tmpi = -1;
     char *calc = malloc(DEFAULT_SIZE * sizeof(char));
@@ -61,14 +61,14 @@ int plot(char *input, int start) {
         }
     }
 
-    intervall = (max - min) / WIDTH;
+    intervall = (max - min) / (WIDTH);
 
-    for (i = 0; i < WIDTH; ++i) {
+    for (i = 0; i <= WIDTH; ++i) {
         x = min + i * intervall;
         y[i] = solve(calc, (int) strlen(calc));
     }
 
-    for (i = 0; i < WIDTH; ++i) {
+    for (i = 0; i <= WIDTH; ++i) {
         if (y[i] <= ymin)
             ymin = y[i];
         if (y[i] >= ymax)
@@ -77,42 +77,45 @@ int plot(char *input, int start) {
     if (ymax == ymin) {
         ymax = fabs(ymax) * 2;
         ymin = fabs(ymin) * -2;
-        yIntervall = (ymax - ymin) / HEIGHT;
-    }
-    else if (fabs(ymin) > fabs(ymax)) {
-        yIntervall = (2 * fabs(ymin)) / HEIGHT;
+        yIntervall = (ymax - ymin) / (HEIGHT);
     }
     else {
-        yIntervall = (2 * fabs(ymax)) / HEIGHT;
+        yIntervall = (ymax - ymin) / (HEIGHT);
     }
 
     printf("y-intervall: %f\nx-intervall: %f\n", yIntervall * Y_INTERVALL, intervall * X_INTERVALL);
 
 
-    for (i = HEIGHT / 2; i >= -HEIGHT / 2; --i) {
-        for (j = -WIDTH / 2; j < WIDTH / 2; ++j) {
-            if (y[j + WIDTH / 2] <= (i + 0.5) * yIntervall && y[j + WIDTH / 2] >= (i - 0.5) * yIntervall) {
+    for (i = 0; i <= HEIGHT; ++i) {
+        for (j = 0; j <= WIDTH; ++j) {
+            if (y[j] >= ymax - (i + 0.5) * yIntervall && y[j] <= ymax - (i - 0.5) * yIntervall) {
                 //current point corresponds to a value in vector y
                 printf("\x1b[34m*\x1b[0m");    //print * for now in blue color
 
             }
-            else if (i == 0 && j == 0)      // cross at origin
-                wprintf(L"%lc", 0x254B);
-            else if (j == 0) {            // draw vertical axes - y
+            else if (j == 0 || j == WIDTH) {            // draw vertical axes - y    // draw mark every 5 rows
                 if (i % Y_INTERVALL == 0)
-                    wprintf(L"%lc", 0x2524);    // draw mark every 3 rows
+                    wprintf(L"%lc", 0x253C);
                 else
                     wprintf(L"%lc", 0x250A);    //draw normal y axes
             }
-            else if (i == 0) {           // draw horizontal axes - x
+            else if (i == 0 || i == HEIGHT) {           // draw horizontal axes - x
                 if (j % X_INTERVALL == 0)
-                    wprintf(L"%lc", 0x252C);    // draw mark at x axes
+                    wprintf(L"%lc", 0x253C);    // draw mark at x axes
                 else
                     wprintf(L"%lc", 0x2508);    // draw normal x axes
             }
+            else if (j % X_INTERVALL == 0 && i % Y_INTERVALL == 0) // draw gray grid
+                wprintf(L"%lc", 0x253C);
+            else if (j % X_INTERVALL == 0)
+                wprintf(L"%lc", 0x250A);
+            else if (i % Y_INTERVALL == 0)
+                wprintf(L"%lc", 0x2508);
             else
                 printf(" ");
 
+            if (i % Y_INTERVALL == 0 && j == WIDTH) // numbers for y axis
+                wprintf(L"%.2f", ymax - i * yIntervall);
 
         }
         printf("\n");
